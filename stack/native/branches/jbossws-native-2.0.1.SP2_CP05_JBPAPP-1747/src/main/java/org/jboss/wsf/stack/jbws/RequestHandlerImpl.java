27d00704ab6a8d4c2fa60e8ef0e73da2cb3e10b7
darran.lofthouse@jboss.com <darran.lofthouse@jboss.com@50344849-fb0f-0410-a53f-a94597aac05c>
2009-03-06 17:26:51 +0000

    [JBPAPP-1747] Fault handler chain is not called when user application exceptions are thrown.

--- stack/native/branches/jbossws-native-2.0.1.SP2_CP05_JBPAPP-1747/src/main/java/org/jboss/wsf/stack/jbws/RequestHandlerImpl.java
+++ stack/native/branches/jbossws-native-2.0.1.SP2_CP05_JBPAPP-1747/src/main/java/org/jboss/wsf/stack/jbws/RequestHandlerImpl.java
@@ -433,7 +433,7 @@ public class RequestHandlerImpl implements RequestHandler
       }
       catch (Exception ex)
       {
-         MessageAbstraction resMessage = msgContext.getMessageAbstraction();
+         MessageAbstraction resMessage = MessageContextAssociation.peekMessageContext().getMessageAbstraction();
 
          // In case we have an exception before the invoker is called
          // we create the fault message here.
@@ -453,7 +453,7 @@ public class RequestHandlerImpl implements RequestHandler
       {
          try
          {
-            MessageAbstraction resMessage = msgContext.getMessageAbstraction();
+            MessageAbstraction resMessage = MessageContextAssociation.peekMessageContext().getMessageAbstraction();
             if (resMessage != null)
             {
                if (resMessage.isFaultMessage())
