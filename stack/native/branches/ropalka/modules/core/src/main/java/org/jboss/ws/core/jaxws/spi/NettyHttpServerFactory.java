c889c8e56e7f53da3d2e55c5786b3e844ea5580c
richard.opalka@jboss.com <richard.opalka@jboss.com@50344849-fb0f-0410-a53f-a94597aac05c>
2009-09-29 08:31:23 +0000

    [JBWS-2674][JBWS-2754] refactoring (WIP)

---
+++ stack/native/branches/ropalka/modules/core/src/main/java/org/jboss/ws/core/jaxws/spi/NettyHttpServerFactory.java
@@ -0,0 +1,54 @@
+/*
+ * JBoss, Home of Professional Open Source.
+ * Copyright 2009, Red Hat Middleware LLC, and individual contributors
+ * as indicated by the @author tags. See the copyright.txt file in the
+ * distribution for a full listing of individual contributors.
+ *
+ * This is free software; you can redistribute it and/or modify it
+ * under the terms of the GNU Lesser General Public License as
+ * published by the Free Software Foundation; either version 2.1 of
+ * the License, or (at your option) any later version.
+ *
+ * This software is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU
+ * Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public
+ * License along with this software; if not, write to the Free
+ * Software Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA
+ * 02110-1301 USA, or see the FSF site: http://www.fsf.org.
+ */
+package org.jboss.ws.core.jaxws.spi;
+
+import org.jboss.wsf.spi.http.HttpServer;
+import org.jboss.wsf.spi.http.HttpServerFactory;
+
+/**
+ * @see org.jboss.wsf.spi.http.HttpServerFactory 
+ *
+ * @author <a href="mailto:ropalka@redhat.com">Richard Opalka</a>
+ */
+public final class NettyHttpServerFactory extends HttpServerFactory
+{
+
+   /**
+    * Constructor.
+    */
+   public NettyHttpServerFactory()
+   {
+      super();
+   }
+   
+   /**
+    * Returns Netty based http server.
+    * 
+    * @return Netty based http server
+    */
+   @Override
+   public HttpServer getHttpServer()
+   {
+      throw new UnsupportedOperationException();
+   }
+
+}
