e69150ff45dba6bbadf4f68d025b1f98602b6a10
alessio.soldano@jboss.com <alessio.soldano@jboss.com@50344849-fb0f-0410-a53f-a94597aac05c>
2010-03-22 17:53:56 +0000

    Tagging jbossws-native-3.3.0.CR1

---
+++ stack/native/tags/jbossws-native-3.3.0.CR1/modules/testsuite/native-tests/src/test/java/org/jboss/test/ws/jaxws/samples/dar/AddressingClientTestCase.java
@@ -0,0 +1,104 @@
+/*
+ * JBoss, Home of Professional Open Source.
+ * Copyright 2006, Red Hat Middleware LLC, and individual contributors
+ * as indicated by the @author tags. See the copyright.txt file in the
+ * distribution for a full listing of individual contributors.
+ *
+ * This is free software; you can redistribute it and/or modify it
+ * under the terms of the GNU Lesser General Public License as
+ * published by the Free Software Foundation; either version 2.1 of
+ * the License, or (at your option) any later version.
+ *
+ * This software is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU
+ * Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public
+ * License along with this software; if not, write to the Free
+ * Software Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA
+ * 02110-1301 USA, or see the FSF site: http://www.fsf.org.
+ */
+package org.jboss.test.ws.jaxws.samples.dar;
+
+import java.net.URL;
+import java.util.Date;
+
+import javax.management.ObjectName;
+
+import junit.framework.Test;
+
+import org.jboss.wsf.common.ObjectNameFactory;
+import org.jboss.wsf.test.JBossWSTest;
+import org.jboss.wsf.test.JBossWSTestHelper;
+import org.jboss.wsf.test.JBossWSTestSetup;
+
+/**
+ * DAR addressing client; invokes the DAR addressing endpoint (sync, asynch and oneway)
+ *
+ * @author Thomas.Diesler@jboss.org
+ * @since 24-Nov-2005
+ */
+public class AddressingClientTestCase extends JBossWSTest
+{
+   private static final int TEST_RUN_TIME = 6000;
+   
+   public static Test suite()
+   {
+      //TODO: replace isHornetQAvailable call with JBossWSTestHelper.isTargetJBoss6() once AS 6 M3 is out and hence M2 is not supported anymore
+      return new JBossWSTestSetup(AddressingClientTestCase.class, isHornetQAvailable() ? "jaxws-samples-dar-queue.sar,jaxws-samples-dar-addressing-client.war,jaxws-samples-dar-addressing.jar" : "jaxws-samples-dar-addressing-client.war,jaxws-samples-dar-addressing.jar");
+   }
+   
+   private static boolean isHornetQAvailable()
+   {
+      try
+      {
+         ObjectName oname = ObjectNameFactory.create("jboss.system:type=Server");
+         String jbossVersion = (String)getServer().getAttribute(oname, "VersionNumber");
+         return JBossWSTestHelper.isTargetJBoss6() && !jbossVersion.contains("M2");
+      }
+      catch (Exception e)
+      {
+         return false;
+      }
+   }
+
+   public void testSync() throws Exception
+   {
+      URL wsdlURL = new URL("http://" + getServerHost() + ":8080/dar?wsdl");
+      AddressingClient client = new AddressingClient(wsdlURL, getServerHost());
+      Date start = new Date();
+      client.run(false);
+      Date stop = new Date();
+      assertTrue(stop.getTime() - start.getTime() > TEST_RUN_TIME / 2);
+   }
+   
+   public void testAsync() throws Exception
+   {
+      URL wsdlURL = new URL("http://" + getServerHost() + ":8080/dar?wsdl");
+      AddressingClient client = new AddressingClient(wsdlURL, getServerHost());
+      Date start = new Date();
+      client.run(true);
+      Date stop = new Date();
+      assertTrue(stop.getTime() - start.getTime() > TEST_RUN_TIME / 2);
+   }
+   
+   public void testOneWay() throws Exception
+   {
+      URL wsdlURL = new URL("http://" + getServerHost() + ":8080/dar?wsdl");
+      long count = getRequestCount("dar-client", "ReplyService");
+      AddressingClient client = new AddressingClient(wsdlURL, getServerHost());
+      Date start = new Date();
+      client.runOneway();
+      Date stop = new Date();
+      assertTrue(stop.getTime() - start.getTime() < TEST_RUN_TIME / 2);
+      Thread.sleep(TEST_RUN_TIME);
+      assertEquals(count + 1, getRequestCount("dar-client", "ReplyService"));
+   }
+   
+   private long getRequestCount(String context, String endpoint) throws Exception
+   {
+      ObjectName oname = ObjectNameFactory.create("jboss.ws:context=" + context + ",endpoint=" + endpoint);
+      return (Long)getServer().getAttribute(oname, "RequestCount");
+   }
+}
