d7dba45264f3976d94db9075fb08c03069601af6
richard.opalka@jboss.com <richard.opalka@jboss.com@50344849-fb0f-0410-a53f-a94597aac05c>
2012-08-28 12:43:31 +0000

    tagging JBossWS Native 4.1.0.Beta2

---
+++ stack/native/tags/jbossws-native-4.1.0.Beta2/modules/testsuite/native-tests/src/test/java/org/jboss/test/ws/jaxrpc/jbws643/JBWS643TestCase.java
@@ -0,0 +1,90 @@
+/*
+ * JBoss, Home of Professional Open Source.
+ * Copyright 2006, Red Hat Middleware LLC, and individual contributors
+ * as indicated by the @author tags. See the copyright.txt file in the
+ * distribution for a full listing of individual contributors.
+ *
+ * This is free software; you can redistribute it and/or modify it
+ * under the terms of the GNU Lesser General Public License as
+ * published by the Free Software Foundation; either version 2.1 of
+ * the License, or (at your option) any later version.
+ *
+ * This software is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU
+ * Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public
+ * License along with this software; if not, write to the Free
+ * Software Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA
+ * 02110-1301 USA, or see the FSF site: http://www.fsf.org.
+ */
+package org.jboss.test.ws.jaxrpc.jbws643;
+
+import javax.naming.InitialContext;
+import javax.xml.rpc.Service;
+
+import junit.framework.Test;
+
+import org.jboss.wsf.test.CleanupOperation;
+import org.jboss.wsf.test.JBossWSTest;
+import org.jboss.wsf.test.JBossWSTestSetup;
+
+
+
+/**
+ * Enhanced guessing of bean properties in BeanDeserializer
+ * 
+ * http://jira.jboss.org/jira/browse/JBWS-643
+ *
+ * @author Thomas.Diesler@jboss.org
+ * @since 27-Jan-2006
+ */
+public class JBWS643TestCase extends JBossWSTest
+{
+   private static ResearchService port;
+   private static InitialContext iniCtx;
+   
+   /** Deploy the test */
+   public static Test suite() throws Exception
+   {
+      return new JBossWSTestSetup(JBWS643TestCase.class, "jaxrpc-jbws643.war, jaxrpc-jbws643-appclient.ear#jaxrpc-jbws643-appclient.jar", new CleanupOperation() {
+         @Override
+         public void cleanUp() {
+            port = null;
+         }
+      });
+   }
+
+   protected void setUp() throws Exception
+   {
+      super.setUp();
+      if (port == null)
+      {
+         iniCtx = getAppclientInitialContext();
+         Service service = (Service)iniCtx.lookup("java:service/ResearchService");
+         port = (ResearchService)service.getPort(ResearchService.class);
+      }
+   }
+
+   protected void tearDown() throws Exception
+   {
+      if (iniCtx != null)
+      {
+         iniCtx.close();
+      }
+      super.tearDown();
+   }
+
+   public void testCreateService() throws Exception
+   {
+      assertNotNull("port not null", port);
+   }
+   
+   public void testSendMessage() throws Exception
+   {
+      GetKeywordStatisticsRequest reqObj = new GetKeywordStatisticsRequest(new String[]{"key1","key2"}, "market", false);
+      GetKeywordStatisticsResponse resObj = port.getKeywordStatistics(reqObj);
+      assertNotNull("response not null", resObj);
+   }
+}
